7e352b2b38bd99ea073fb9c78b8a7c7549e65174
Barrett Pope <barrettpope94@gmail.com>
2020-09-11 14:16:45 -0400

    Methods added to ORM file

--- config/orm.js
+++ config/orm.js
@@ -2,23 +2,31 @@ const connection = require("./connection")
 
 let orm = {
 
-    selectAll: function (tableInput) {
+    selectAll: function (tableInput, callback) {
         let queryString = "SELECT * FROM ?"
-        connection.query(queryString, tableInput),
+        connection.query(queryString, [tableInput],
             function (err, result) {
                 if (err) throw err;
-                console.log(result);
-            };
+                callback(result);
+            });
     },
 
-    insertOne: function () {
-
+    insertOne: function (burgerInput, callback) {
+        const queryString = "INSERT INTO burgers (burger_name, devoured) VALUES (?,false)";
+        connection.query(queryString, [burgerInput], function (err, result) {
+            if (err) throw err;
+            callback(result);
+        });
     },
 
-    updateOne: function () {
-
+    updateOne: function (id, callback) {
+        const queryString = "UPDATE burgers SET devoured = true WHERE id = ?";
+        connection.quuery(queryString, [id], function (err, result) {
+            if (err) throw err;
+            callback(result);
+        });
     }
 
-}
+};
 
 module.exports = orm;
