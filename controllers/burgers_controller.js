a7144c52b8b5362456d79a96399efd9c8d382210
Barrett Pope <barrettpope94@gmail.com>
2020-09-12 09:32:54 -0400

    Added put method

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,25 @@
 const express = require("express");
 const burger = require("../models/burger");
+const router = express.Router();
 
-var router = express.Router();
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        res.render("index", {
+            burger: data
+        });
+    })
+});
+
+router.post("/api/burger", function (req, res) {
+    burger.insertOne(req.body.burgerInput, function (data) {
+        res.redirect("/")
+    })
+});
+
+router.put("/api/burger/:id", function (req, res) {
+    burger.updateOne(req.params.id, function (data) {
+        res.redirect("/")
+    })
+});
+
+module.exports = router;
